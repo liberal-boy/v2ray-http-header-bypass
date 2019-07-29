0f3d86379c389674b35751f7e097d9a48b3d1945
liberal-boy <liberal-boy@protonmail.com>
2019-07-29 21:38:35 +0800

    allowed tls12, tls13 and AEAD cipher only

--- main.go
+++ main.go
@@ -45,7 +45,22 @@ func listenTls() (ln net.Listener, err error) {
 	if err != nil {
 		return
 	}
-	config := &tls.Config{Certificates: []tls.Certificate{cert}}
+	config := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		MinVersion:   tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+		},
+	}
 	ln, err = tls.Listen("tcp", listen, config)
 	return
 }
